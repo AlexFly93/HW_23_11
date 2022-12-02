494572c6a4aefc17c52d8bf80deec38c0e3164a6
AlexFly93 <muhabon93@gmail.com>
2022-12-02 16:32:33 +0200

    Add variant from internet

--- index.js
+++ index.js
@@ -2,12 +2,12 @@
 // Результат буде [1,2,5]
 // Якщо в джерельному масиві усі значення унікальні, то створюєте новий пустий масив.
 
-const nums = [1, 2, 3, 1, 5, 6, 1, 2, 5]
+// const nums = [1, 2, 3, 1, 5, 6, 1, 2, 5]
 
-function getSameNums(value, index, self) {
-  return self.indexOf(value) !== index;
-}
-const sameNums = nums.filter(getSameNums).splice(1);
+// function getSameNums(value, index, self) {
+//   return self.indexOf(value) !== index;
+// }
+// const sameNums = nums.filter(getSameNums).splice(1);
 // function getSameNums(array) {
 //  let sameNums = [];
 //     for (let i = 0; i < array.length; i++) {
@@ -16,3 +16,20 @@ const sameNums = nums.filter(getSameNums).splice(1);
 //         }
 //     }return sameNums;
 // }
+
+
+//Вариант с инета
+
+    const nums = [1, 2, 3, 1, 5, 6, 1, 2, 5];
+
+    const uniqSet = new Set();
+    for (let i = 0; i < nums.length; i++) {
+      for (let j = 0; j < i; j++)
+        if (nums[j] === nums[i]) {
+          uniqSet.add(nums[i]);
+        }
+    }
+
+    const uniq = Array.from(uniqSet);
+
+    console.log(uniq);
